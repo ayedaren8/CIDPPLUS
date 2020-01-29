f8c73e3ab5715f35da0271599f1d371a63cd6f50
ayedaren <1783580635@qq.com>
2020-01-29 23:49:20 +0800

    加了三个函数

--- app.js
+++ app.js
@@ -1,43 +1,52 @@
 //app.js
 App({
-  onLaunch: function () {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
+    onLaunch: function() {
+        // 展示本地存储能力
+        var logs = wx.getStorageSync('logs') || []
+        logs.unshift(Date.now())
+        wx.setStorageSync('logs', logs)
 
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
+        // 登录
+        wx.login({
+                success: res => {
+                    // 发送 res.code 到后台换取 openId, sessionKey, unionId
+                }
+            })
+            // 获取用户信息
+        wx.getSetting({
             success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
+                if (res.authSetting['scope.userInfo']) {
+                    // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+                    wx.getUserInfo({
+                        success: res => {
+                            // 可以将 res 发送给后台解码出 unionId
+                            this.globalData.userInfo = res.userInfo
 
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
+                            // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+                            // 所以此处加入 callback 以防止这种情况
+                            if (this.userInfoReadyCallback) {
+                                this.userInfoReadyCallback(res)
+                            }
+                        }
+                    })
+                }
             }
-          })
-        }
-      }
-    })
-  },
-  globalData: {
-    userInfo: null,
-    USERNAME:null,
-    LOGIN_FLAG:null,
-    PROCESS:"正在加载"
-
-  }
+        })
+    },
+    globalData: {
+        userInfo: null,
+        USERNAME: null,
+        LOGIN_FLAG: null,
+        PROCESS: "",
+        theme_main_color: "#19896A",
+        theme_secondary_color: "#ffffff",
+        LOGIN_FLAG: false
+    },
+    Login_in: () => {
+        this.globalData.LOGIN_FLAG = true
+    },
+    login_out: () => {
+        this.globalData.LOGIN_FLAG = false
+    },
+    isLogin: () => this.globalData.LOGIN_FLAG
 })
