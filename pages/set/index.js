e56b8e4e0670f82827fd0f639b293085561f2f53
ayedaren <1783580635@qq.com>
2020-03-03 22:26:09 +0800

    增加set页面

--- pages/set/index.js
+++ pages/set/index.js
@@ -1,18 +1,114 @@
 // pages/set/index.js
+const app = getApp();
+import Dialog from '../../dist/dialog/dialog';
 Page({
 
     /**
      * 页面的初始数据
      */
     data: {
-
+        btnSize: "",
+        notePWD: '',
+        exitRE: '',
+        LOGIN_FLAG: '',
+        USERNAME: "",
+        studentID: "",
+        NowWeek: "",
     },
 
     /**
      * 生命周期函数--监听页面加载
      */
-    onLoad: function(options) {
+    login: function() {
+        wx.navigateTo({
+            url: '/pages/login/login',
+            success: (result) => {
 
+            },
+            fail: () => {},
+            complete: () => {}
+        });
+    },
+    loginOut: function() {
+        Dialog.confirm({
+            title: '注销',
+            message: '是否注销？'
+        }).then(() => {
+            app.globalData.LOGIN_FLAG = false
+            this.setData({ LOGIN_FLAG: app.globalData.LOGIN_FLAG })
+            wx.removeStorage({
+                key: 'grade',
+                success: (result) => {
+                    console.log("成功删除成绩缓存")
+                },
+                fail: () => {},
+                complete: () => {}
+            });
+            wx.removeStorage({
+                key: 'course',
+                success: (result) => {
+                    console.log("成功删除课表缓存")
+                },
+                fail: () => {},
+                complete: () => {}
+            });
+            wx.removeStorage({
+                key: 'exam',
+                success: (result) => {
+                    console.log("成功删除课表缓存")
+                },
+                fail: () => {},
+                complete: () => {}
+            });
+        }).catch(() => {
+            // on cancel
+        });
+    },
+    onChange_notePWD: function() {
+        app.globalData.notePWD = this.data.notePWD == true ? false : true
+        console.log(app.globalData.notePWD);
+        this.setData({ notePWD: app.globalData.notePWD })
+        wx.setStorage({
+            key: "set",
+            data: {
+                LOGIN_FLAG: app.globalData.LOGIN_FLAG,
+                notePWD: app.globalData.notePWD,
+                exitRE: app.globalData.exitRE
+            }
+        });
+    },
+    onChange_exitRE: function() {
+        Dialog.confirm({
+            title: '确定吗？',
+            message: '打开此选项会导致程序加载时间延长，但数据更新会更及时。'
+        }).then(() => {
+            app.globalData.exitRE = this.data.exitRE == true ? false : true
+            this.setData({ exitRE: app.globalData.exitRE })
+            wx.setStorage({
+                key: "set",
+                data: {
+                    LOGIN_FLAG: app.globalData.LOGIN_FLAG,
+                    notePWD: app.globalData.notePWD,
+                    exitRE: app.globalData.exitRE
+                }
+            });
+
+        }).catch(() => {
+            // on cancel
+        });
+    },
+
+    onLoad: function(options) {
+        this.setData({ notePWD: app.globalData.notePWD })
+        this.setData({ exitRE: app.globalData.exitRE })
+        this.setData({ btnSize: 48 * app.globalData.RPX })
+    },
+    toPage: function(data) {
+        let title = data.currentTarget.dataset.title
+        console.log(data.currentTarget.dataset.title);
+        wx.navigateTo({
+            url: '/pages/page/page?title=' + title
+        })
     },
 
     /**
@@ -33,6 +129,13 @@ Page({
                 selected: 4
             })
         }
+        let res = wx.getStorageSync("infoList");
+        console.log(res);
+        this.setData({
+            LOGIN_FLAG: app.globalData.LOGIN_FLAG,
+            USERNAME: res.name,
+            studentID: res.srNum
+        })
     },
 
     /**
