0c5e0e677b2444c8571fffaa34cba726c1d20843
ayedaren8 <1783580635@qq.com>
2020-04-20 23:44:12 +0800

    加入校庆头像生成功能

--- pages/index/index.js
+++ pages/index/index.js
@@ -358,7 +358,14 @@ Page({
      * 页面相关事件处理函数--监听用户下拉动作
      */
     onPullDownRefresh: function() {
-
+        wx.navigateTo({
+            url: '/pages/product/index',
+            success: (result) => {
+            },
+            fail: () => {},
+            complete: () => {}
+        });
+    
     },
 
     /**
